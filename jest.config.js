44a6861dc25c63a9999ad01274f0d1a5c1dec1a4
yashwant-kaplan <yashwant.raghuwanshi@kaplan.com>
2021-06-24 17:38:20 +0530

    added auth lib

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,7 @@ module.exports = {
     '<rootDir>/apps/admin',
     '<rootDir>/apps/customer',
     '<rootDir>/libs/shared',
+    '<rootDir>/apps/login-demo-app',
+    '<rootDir>/libs/auth',
   ],
 };
